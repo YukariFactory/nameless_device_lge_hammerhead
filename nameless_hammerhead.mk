a42135b5ea89381073dd133d08c4834627c9cd1c
droidfivex <droidfivex@gmail.com>
2016-07-05 06:48:17 +0000

    for namelessrom 3.0

--- nameless_hammerhead.mk
+++ nameless_hammerhead.mk
@@ -3,18 +3,24 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 TARGET_BOOTANIMATION_HALF_RES := true
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit APNs list
+$(call inherit-product, vendor/nameless/config/apns.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+
+# Inherit from our custom product configuration
+$(call inherit-product, vendor/nameless/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/hammerhead/full_hammerhead.mk)
 
+# Enhanced NFC
+$(call inherit-product, vendor/nameless/config/nfc_enhanced.mk)
+
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := hammerhead
-PRODUCT_NAME := cm_hammerhead
+PRODUCT_NAME := nameless_hammerhead
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 5
 PRODUCT_MANUFACTURER := LGE
@@ -23,3 +29,9 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=hammerhead \
     BUILD_FINGERPRINT=google/hammerhead/hammerhead:6.0.1/MOB30M/2862625:user/release-keys \
     PRIVATE_BUILD_DESC="hammerhead-user 6.0.1 MOB30M 2862625 release-keys"
+
+# Inherit the SIM Toolkit
+PRODUCT_PACKAGES += Stk
+
+# Enable Torch
+PRODUCT_PACKAGES += Torch
